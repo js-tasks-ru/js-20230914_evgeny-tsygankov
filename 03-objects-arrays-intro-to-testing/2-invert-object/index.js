10c2bc9123476df49f9dbe6515721d029860ab32
Evgeny <evtsyga@gmail.com>
2023-09-21 20:57:59 +0300

    add invertObj implementation

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,13 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  if (!obj) {
+    return undefined;
+  }
 
+  const resultObject = {};
+  for (const key in obj) {
+    resultObject[obj[key]] = key;
+  }
+  return resultObject;
 }
