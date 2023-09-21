f6a77e72b5e3a4cb803b316fb80ac83285074417
Evgeny <evtsyga@gmail.com>
2023-09-21 20:48:56 +0300

    add createGetter implementation

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -1,8 +1,31 @@
+import { isObject } from "./utils";
+
 /**
  * createGetter - creates function getter which allows select value from object
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const parsedPath = path.split(".");
 
+  const inner = (obj) => {
+    let result = { ...obj };
+
+    for (const key of parsedPath) {
+      if (!(key in result)) {
+        result = undefined;
+        break;
+      }
+      const value = result[key];
+      if (!isObject(value)) {
+        result = value;
+        break;
+      }
+      result = { ...value };
+    }
+
+    return result;
+  };
+
+  return inner;
 }
