7e7cfedc423ce72e0111f0e1216842fac9d7a803
Evgeny <evtsyga@gmail.com>
2023-09-23 22:13:16 +0300

    add trimSymbols implementation

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -1,3 +1,5 @@
+import { isSameAsPrev } from "./utils";
+
 /**
  * trimSymbols - removes consecutive identical symbols if they quantity bigger that size
  * @param {string} string - the initial string
@@ -5,5 +7,30 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) {
+    return "";
+  }
+  if (!size) {
+    return string;
+  }
 
+  let res = "";
+  let counter = 1;
+
+  for (let i = 0; i <= string.length - 1; i++) {
+    const prevChar = string[i - 1];
+    const currentChar = string[i];
+
+    if (isSameAsPrev(prevChar, currentChar)) {
+      if (counter < size) {
+        res += currentChar;
+        counter += 1;
+      }
+    } else {
+      res += currentChar;
+      counter = 1;
+    }
+  }
+
+  return res;
 }
